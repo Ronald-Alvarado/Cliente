f9b92db78fbc32f76df2b17096e6ef0a3d9b1441
ronal <ronal@DESKTOP-RLIPRAX>
2021-09-19 00:18:16 -0400

    Add project files.

---
+++ Cliente/Entidades/Citas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cliente.Entidades
+{
+    public class Citas
+    {
+        public int CitasId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaCita { get; set; }
+        public string Hora { get; set; }
+
+
+        public Citas()
+        {
+            CitasId = 0;
+            Nombres = string.Empty;
+            Apellidos = string.Empty;
+            Telefono = string.Empty;
+            Direccion = string.Empty;
+            FechaCita = DateTime.Now;
+            Hora = string.Empty;
+        }
+    }
+}
